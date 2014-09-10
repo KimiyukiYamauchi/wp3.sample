7d86a919bfd050e216f0338ce9a0241b8403a4d1
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-09-10 21:30:41 +0900

    ヘッダーとフッターの作成

--- footer.php
+++ footer.php
@@ -1,3 +1,9 @@
+<div id="footer">
+<p>Copyright &copy; <?php bloginfo('name'); ?>, All rights reserved.</p>
+
+</div><!-- #footer -->
+
+</div><!-- #container -->
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,8 +1,20 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-	<meta charset="UTF-8">
-	<title></title>
+<meta charset="<?php bloginfo('charset'); ?>">
+<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
 </head>
 <body>
 
+<div id="container">
+
+
+<div id="header">
+<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+<p id="desc"><?php bloginfo('description'); ?></p>
+
+<div id="subinfo">
+<a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('template_url'); ?>/feed.png" alt="RSS FEED" width="28" height="28" /></a>
+</div><!-- #subinfo -->
+</div><!-- #header -->
