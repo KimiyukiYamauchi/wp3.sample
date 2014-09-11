94444aafc57eda478181d791f7f72ef0ef8841af
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-09-11 20:58:08 +0900

    p40上まで

--- content.php
+++ content.php
@@ -1,5 +1,10 @@
+<div class="post">
 <h2>
 <a href="<?php the_permalink(); ?>">
 <?php the_title(); ?>
 </a>
 </h2>
+<p class="postcat"><?php the_category(' '); ?></p>
+
+<?php the_content(); ?>
+</div>
