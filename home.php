f659f7a5aea7e2d0c1548eb2e5e52db67c6a2d2b
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-09-17 19:04:22 +0900

    p42上

--- home.php
+++ home.php
@@ -1,7 +1,11 @@
 <?php get_header(); ?>
 
+<p class="title">RECENT POSTS</p>
+
 <?php if(have_posts()): while(have_posts()): the_post() ?>
 	<?php get_template_part('content'); ?>
 <?php endwhile; endif; ?>
+
+<?php get_template_part('pagenation'); ?>
 
 <?php get_footer(); ?>
