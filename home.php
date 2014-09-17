0c76386e62038f82189b66a12815bcfbb700aa81
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-09-17 19:15:45 +0900

    p43まで

--- home.php
+++ home.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
 
-<p class="title">RECENT POSTS</p>
-
-<?php if(have_posts()): while(have_posts()): the_post() ?>
-	<?php get_template_part('content'); ?>
-<?php endwhile; endif; ?>
-
-<?php get_template_part('pagenation'); ?>
+<div id="content">
+  <p class="title">RECENT POSTS</p>
+  
+  <?php if(have_posts()): while(have_posts()): the_post() ?>
+  	<?php get_template_part('content'); ?>
+  <?php endwhile; endif; ?>
+  
+  <?php get_template_part('pagenation'); ?>
+</div><!-- content -->
 
 <?php get_footer(); ?>
